8ea51e62200fa8e343c29e8b453a2328df4f6a01
Arthur Renê <arthurkau12@hotmail.com>
2023-10-28 15:19:03 -0300

    Update modulo1.py

--- modulo1.py
+++ modulo1.py
@@ -1,69 +1,39 @@
 import csv
 
 class Estoque:
-    def __init__(self, gasolina, alcool, diesel, energia_solar, limite_minimo=100):
-        self.gasolina = gasolina
-        self.alcool = alcool
-        self.diesel = diesel
-        self.energia_solar = energia_solar
+    def __init__(self, nome_produto, quantidade_inicial, limite_minimo):
+        self.nome_produto = nome_produto
+        self.quantidade = quantidade_inicial
         self.limite_minimo = limite_minimo
-        self.filename = 'estoque_pecas.csv'
 
-    def receber_produto(self, tipo, quantidade):
-        if tipo == 'gasolina':
-            self.gasolina += quantidade
-        elif tipo == 'alcool':
-            self.alcool += quantidade
-        elif tipo == 'diesel':
-            self.diesel += quantidade
-        elif tipo == 'energia_solar':
-            self.energia_solar += quantidade
+    def receber_produto(self, quantidade):
+        self.quantidade += quantidade
+        self.atualizar_csv()
+        print(f"Recebido {quantidade} de {self.nome_produto}. Estoque atual: {self.quantidade}")
+
+    def vender_produto(self, quantidade):
+        if self.quantidade >= quantidade:
+            self.quantidade -= quantidade
+            self.atualizar_csv()
+            print(f"Vendido {quantidade} de {self.nome_produto}. Estoque atual: {self.quantidade}")
         else:
-            print("Tipo de produto inválido.")
+            print(f"Estoque insuficiente para vender {quantidade} de {self.nome_produto}.")
 
-        self.atualizar_arquivo()
+    def verificar_estoque(self):
+        if self.quantidade <= self.limite_minimo:
+            print(f"ALERTA: Estoque de {self.nome_produto} está abaixo do limite mínimo de {self.limite_minimo}!")
 
-    def vender_produto(self, tipo, quantidade):
-        if tipo == 'gasolina':
-            if self.gasolina >= quantidade:
-                self.gasolina -= quantidade
-            else:
-                print("Quantidade de gasolina insuficiente.")
-        elif tipo == 'alcool':
-            if self.alcool >= quantidade:
-                self.alcool -= quantidade
-            else:
-                print("Quantidade de álcool insuficiente.")
-        elif tipo == 'diesel':
-            if self.diesel >= quantidade:
-                self.diesel -= quantidade
-            else:
-                print("Quantidade de diesel insuficiente.")
-        elif tipo == 'energia_solar':
-            if self.energia_solar >= quantidade:
-                self.energia_solar -= quantidade
-            else:
-                print("Quantidade de energia solar insuficiente.")
-        else:
-            print("Tipo de produto inválido.")
+    def atualizar_csv(self):
+        data = []
 
-        self.atualizar_arquivo()
+        with open('estoque.csv', 'r', newline='') as file:
+            reader = csv.reader(file)
+            for linha in reader:
+                if linha and linha[0] == self.nome_produto and len(linha) >= 2:
+                    linha[1] = str(self.quantidade)
+                data.append(linha)
 
-    def verificar_alerta(self):
-        if self.gasolina < self.limite_minimo:
-            print(f"Alerta: Nível de gasolina abaixo do limite mínimo.")
-        if self.alcool < self.limite_minimo:
-            print(f"Alerta: Nível de álcool abaixo do limite mínimo.")
-        if self.diesel < self.limite_minimo:
-            print(f"Alerta: Nível de diesel abaixo do limite mínimo.")
-        if self.energia_solar < self.limite_minimo:
-            print(f"Alerta: Nível de energia solar abaixo do limite mínimo.")
-
-    def atualizar_arquivo(self):
-        with open(self.filename, mode='w', newline='') as file:
+        with open('estoque.csv', 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Produto', 'Quantidade'])
-            writer.writerow(['Gasolina', self.gasolina])
-            writer.writerow(['Álcool', self.alcool])
-            writer.writerow(['Diesel', self.diesel])
-            writer.writerow(['Energia Solar', self.energia_solar])
+            for linha in data:
+                writer.writerow(linha)
